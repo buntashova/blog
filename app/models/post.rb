960ea2a959523bf49ed3d510338f7e18fc07339f
DX37 <asusx2@mail.ru>
2018-12-23 21:39:06 +0700

    add local comments to blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     validates :title, :summary, :body, presence: true
+    acts_as_commontable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
+  acts_as_commontator
 end
